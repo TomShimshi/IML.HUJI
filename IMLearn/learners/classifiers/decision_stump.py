e0962ccb00d40547a54f3e71e54b6b9515d993e7
TomShimshi <shimshitom@gmail.com>
2022-05-24 11:18:15 +0300

    Ex4

--- IMLearn/learners/classifiers/decision_stump.py
+++ IMLearn/learners/classifiers/decision_stump.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Tuple, NoReturn
-from ...base import BaseEstimator
+from IMLearn.base.base_estimator import BaseEstimator
+# from ...base import BaseEstimator
 import numpy as np
 from itertools import product
 
@@ -39,7 +40,12 @@ class DecisionStump(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        best_loss = None
+        for sign, j in product([-1, 1], range(X.shape[1])):
+            loss, thr = self._find_threshold(X[:, j], y, sign)
+            if best_loss is None or loss < best_loss:
+                self.sign_, self.threshold_, self.j_ = sign, thr, j
+                best_loss = loss
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -63,7 +69,7 @@ class DecisionStump(BaseEstimator):
         Feature values strictly below threshold are predicted as `-sign` whereas values which equal
         to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        return self.sign_ * ((X[:, self.j_] >= self.threshold_) * 2 - 1)
 
     def _find_threshold(self, values: np.ndarray, labels: np.ndarray, sign: int) -> Tuple[float, float]:
         """
@@ -95,7 +101,30 @@ class DecisionStump(BaseEstimator):
         For every tested threshold, values strictly below threshold are predicted as `-sign` whereas values
         which equal to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        sort_idx = np.argsort(values)
+        values, labels, D = values[sort_idx], np.sign(labels)[sort_idx], np.abs(labels)[sort_idx]
+        thetas = np.concatenate([[-np.inf], (values[1:] + values[:-1]) / 2, [np.inf]])
+        minimal_theta_loss = np.sum(D[labels == sign])
+        losses = np.append(minimal_theta_loss, minimal_theta_loss - np.cumsum(D * (labels * sign)))
+        min_loss_idx = np.argmin(losses)
+        return losses[min_loss_idx], thetas[min_loss_idx]
+
+        # min_loss = 0
+        # curr_loss = 0
+        # min_idx = 0
+        # for i in range(values.shape[0]):
+        #     y_pred = np.zeros(values.shape[0])
+        #     for j in range(values.shape[0]):
+        #         if values[j] >= values[i]:
+        #             y_pred[j] = sign
+        #         else:
+        #             y_pred[j] = -sign
+        #     curr_loss = self._loss(labels, y_pred)
+        #     if curr_loss < min_loss:
+        #         min_loss = curr_loss
+        #         min_idx = i
+        #
+        # return values[min_idx], min_loss
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -114,4 +143,5 @@ class DecisionStump(BaseEstimator):
         loss : float
             Performance under missclassification loss function
         """
-        raise NotImplementedError()
+        y_pred = self._predict(X)
+        return float(np.sum(np.sign(y) != np.sign(y_pred)))
--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Tuple
-from IMLearn.learners.metalearners.adaboost import AdaBoost
+from IMLearn.metalearners.adaboost import AdaBoost
 from IMLearn.learners.classifiers import DecisionStump
 from utils import *
 import plotly.graph_objects as go
@@ -37,25 +37,110 @@ def generate_data(n: int, noise_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
     y[np.random.choice(n, int(noise_ratio * n))] *= -1
     return X, y
 
+def decision_surface(t, predict, xrange, yrange, density=120, dotted=False, colorscale=custom, showscale=True):
+    xrange, yrange = np.linspace(*xrange, density), np.linspace(*yrange, density)
+    xx, yy = np.meshgrid(xrange, yrange)
+    pred = predict(np.c_[xx.ravel(), yy.ravel()], t)
+
+    if dotted:
+        return go.Scatter(x=xx.ravel(), y=yy.ravel(), opacity=1, mode="markers", marker=dict(color=pred, size=1, colorscale=colorscale, reversescale=False), hoverinfo="skip", showlegend=False)
+    return go.Contour(x=xrange, y=yrange, z=pred.reshape(xx.shape), colorscale=colorscale, reversescale=False, opacity=.7, connectgaps=True, hoverinfo="skip", showlegend=False, showscale=showscale)
+
 
 def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=500):
-    (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), generate_data(test_size, noise)
+
+    (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), \
+                                           generate_data(test_size, noise)
+
 
     # Question 1: Train- and test errors of AdaBoost in noiseless case
-    raise NotImplementedError()
+    train_errors, test_errors, learners_list = [], [], np.arange(1, n_learners)
+    adaBoost = AdaBoost(DecisionStump, n_learners)
+    adaBoost.fit(train_X, train_y)
+
+    for learner in learners_list:
+        train_errors.append(adaBoost.partial_loss(train_X, train_y, learner))
+        test_errors.append(adaBoost.partial_loss(test_X, test_y, learner))
+
+    fig = make_subplots()
+    fig.add_traces([go.Scatter(x=learners_list, y=train_errors, mode='lines',
+                                   marker=dict(color='blue', line_width=1), name="Train Error"),
+                    go.Scatter(x=learners_list, y=test_errors, mode='lines',
+                                   marker=dict(color='orange', line_width=1), name="Test Error")])
+    fig.update_layout(xaxis_title="Ensemble Size", yaxis_title="Error Rate",
+                      title="Q1 - Adaboost Error",
+                      font=dict(family="Arial", size=20))
+    fig.show()
 
     # Question 2: Plotting decision surfaces
     T = [5, 50, 100, 250]
-    lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    raise NotImplementedError()
+    lims = np.array(
+        [np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array(
+        [-.1, .1])
+
+    fig1 = make_subplots(rows=2, cols=2,
+                         subplot_titles=[f"Ensemble Size of {m}" for m in T],
+                         horizontal_spacing=0.01, vertical_spacing=.03)
+    for i, t in enumerate(T):
+        fig1.add_traces([decision_surface(t, adaBoost.fit(train_X, train_y).partial_predict,
+                                          lims[0], lims[1], showscale=False),
+                         go.Scatter(x=train_X[:, 0], y=train_X[:, 1], mode="markers",
+                                    showlegend=False,
+                                    marker=dict(color=train_y,
+                                                colorscale=[custom[0], custom[-1]],
+                                                line=dict(color="black", width=1)))],
+                        rows=(i // 2) + 1, cols=(i % 2) + 1)
+
+    fig1.update_layout(title="Q2 - Decision Surface of Different Ensemble Size",
+                       font=dict(family="Arial", size=20),
+                       margin=dict(t=100)).update_xaxes(visible=False).update_yaxes(visible=False)
+    fig1.show()
 
     # Question 3: Decision surface of best performing ensemble
-    raise NotImplementedError()
+    test_error = []
+    for T in learners_list:
+        test_error.append(adaBoost.partial_loss(test_X, test_y, T))
+    T_hat = np.argmin(test_error) + 1
+
+    ensemble_size = (T_hat)
+    accuracy = 1 - test_error[T_hat]
+
+    fig2 = make_subplots()
+    fig2.add_traces(
+        [decision_surface(T_hat, adaBoost.fit(train_X, train_y).partial_predict, lims[0], lims[1],
+                          showscale=False),
+         go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers", showlegend=False,
+                    marker=dict(color=test_y,
+                                colorscale=[custom[0], custom[-1]],
+                                line=dict(color="black", width=1)))],
+    )
+
+    fig2.update_layout(title=f"Q3 - Decision Surface of Ensemble Size = {ensemble_size}, Accuracy = {accuracy}",
+                       font=dict(family="Arial", size=20),
+                       margin=dict(t=50))
+    fig2.show()
 
     # Question 4: Decision surface with weighted samples
-    raise NotImplementedError()
+    D = adaBoost.D_
+    D = D / np.max(D) * 5
+    fig3 = make_subplots()
+    fig3.add_traces(
+        [decision_surface(T_hat, adaBoost.fit(train_X, train_y).partial_predict, lims[0], lims[1],
+                          showscale=False),
+         go.Scatter(x=train_X[:, 0], y=train_X[:, 1], mode="markers", showlegend=False,
+                    marker=dict(color=train_y,
+                                colorscale=[custom[0], custom[-1]],
+                                line=dict(color="black", width=1), size=D))],
+    )
+
+    fig3.update_layout(title="Q4 - Decision Surface of Ensemble Size = 250, Size of Markers Proportional to Sample Weight",
+                       font=dict(family="Arial", size=20),
+                       margin=dict(t=100))
+
+    fig3.show()
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    fit_and_evaluate_adaboost(0)
+    fit_and_evaluate_adaboost(0.4)
